cc679fb5c2b905b25c740b17e7085393fd008b23
Thiago Pinho <thiagopinho05@gmail.com>
2022-11-25 20:47:56 -0300

    feat: finalizing modal styling

--- front/src/components/OrderModal/index.tsx
+++ front/src/components/OrderModal/index.tsx
@@ -2,6 +2,7 @@ import { Overlay, ModalBody, OrderDetails } from "./styles";
 
 import closeIcon from "../../assets/images/close-icon.svg";
 import { Order } from "../../types/Order";
+import { formatCurrency } from "../../utils/formatCurrency";
 
 interface OrderModalProps {
   visible: boolean;
@@ -12,6 +13,11 @@ export function OrderModal({ visible, order }: OrderModalProps) {
   if (!visible || !order) {
     return null;
   }
+
+  const total = order.products.reduce((total, { product, quantity }) => {
+    return total + (product.price * quantity);
+  }, 0);
+
   return (
     <Overlay>
       <ModalBody>
@@ -25,13 +31,44 @@ export function OrderModal({ visible, order }: OrderModalProps) {
         <div className="status-container">
           <small>Status do pedido</small>
           <div>
-            <span>🕐</span>
-            <strong>Fila de Espera</strong>
+            <span>
+              {order.status == "WAITING" && "🕐"}
+              {order.status == "IN_PRODUCTION" && "👨‍🍳"}
+              {order.status == "DONE" && "✅"}
+            </span>
+            <strong>
+              {order.status == "WAITING" && "Fila de espera"}
+              {order.status == "IN_PRODUCTION" && "Em preparação"}
+              {order.status == "DONE" && "Pronto"}
+            </strong>
           </div>
 
         </div>
         <OrderDetails>
           <strong>Itens</strong>
+
+          <div className="order-items">
+            {order.products.map(({ _id, product, quantity }) =>
+              <div className="item" key={_id}>
+                <img src={`http://localhost:3002/uploads/${product.image}`}
+                  alt={product.name}
+                  width="48"
+                  height="40">
+                </img>
+                <span className="quantity">{quantity}x</span>
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>{formatCurrency(product.price)}</span>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className="total">
+            <span>Total</span>
+            <strong>{formatCurrency(total)}</strong>
+          </div>
+
         </OrderDetails>
       </ModalBody>
     </Overlay>
--- front/src/components/OrderModal/styles.ts
+++ front/src/components/OrderModal/styles.ts
@@ -61,6 +61,57 @@ export const OrderDetails = styled.div`
     font-weight: 500;
     font-size: 14px;
     opacity: 0.8;
+
+
   }
 
+  .order-items{
+      margin-top: 16px;
+
+      .item{
+        display: flex;
+
+        & + .item{
+          margin-top: 16px;
+        }
+
+        img{
+          border-radius: 6px;
+        }
+
+        .quantity{
+          font-size: 14px;
+          color: #666;
+          display: block;
+          min-width: 20px;
+          margin-left: 12px;
+        }
+        .product-details{
+          margin-left: 4px;
+          strong{
+            display: block;
+            margin-bottom: 4px;
+          }
+
+          span {
+            font-size: 14px;
+            color: #666;
+          }
+        }
+      }
+
+
+    }
+    .total{
+      margin-top: 24px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      span{
+      font-weight: 500;
+      font-size: 14px;
+      opacity: 0.8;
+      }
+    }
 `;
