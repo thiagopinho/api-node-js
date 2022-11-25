b0adbb288a3c232435b3e0c1de4eac081b5f3105
Thiago Pinho <thiagopinho05@gmail.com>
2022-11-25 20:48:52 -0300

    chore: changing type of interface and default values of json

--- front/src/components/Orders/index.tsx
+++ front/src/components/Orders/index.tsx
@@ -7,23 +7,23 @@ const orders: Order[] = [
 
     "_id": "516161",
     "table": "1",
-    "status": "DONE",
+    "status": "WAITING",
     "products": [
       {
         "product": {
           "name": "Pizza",
-          "image": "5165165",
-          "price": 40
+          "image": "1669392827231-pizza.jpg",
+          "price": 30
         },
-        "quantity": 40,
+        "quantity": 4,
         "_id": "84949494"
       }, {
         "product": {
           "name": "Coca Cola",
-          "image": "5165165",
-          "price": 40
+          "image": "1669417474284-cocacola.jpeg",
+          "price": 7
         },
-        "quantity": 40,
+        "quantity": 3,
         "_id": "84949494"
       },
     ]
