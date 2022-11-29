11e98ffe388bb59434a352297b19543328dd29a5
Thiago Pinho <thiagopinho05@gmail.com>
2022-11-29 20:59:14 -0300

    Implementing dadshboard of application

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,15 +1,25 @@
-import { Text } from "../components/Text";
-
-import { Container } from "./styles";
+import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from "./styles";
 
 import { Header } from "../components/Header";
+import { Categories } from "../components/Categories";
+import { Menu } from "../components/Menu";
 
 export function Main() {
   return (
     <>
       <Container>
         <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+
       </Container>
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
     </>
   );
 }
