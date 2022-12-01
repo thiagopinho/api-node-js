973e20738e274dabe4ece5ea6394cdb2a5eacfb4
Thiago Pinho <thiagopinho05@gmail.com>
2022-12-01 19:45:48 -0300

    feat: creating products board UI in dash

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,21 +1,31 @@
 import { FlatList } from "react-native";
 
 import { products } from "../../mocks/products";
+import { formatCurrency } from "../../utils/formatCurrency";
 import { Text } from "../Text";
-import { Product, Image, ProductDetails } from "./styles";
+import { Product, ProductImage, ProductDetails, Separator } from "./styles";
 
 export function Menu() {
   return (
     <FlatList
       data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
       keyExtractor={(product) => product._id}
+      ItemSeparatorComponent={Separator}
       renderItem={({ item: product }) => (
         <Product>
-          <Image />
+          <ProductImage
+            source={{
+              uri: `http://192.168.0.42:3002/uploads/${product.imagePath}`,
+            }}
+          />
           <ProductDetails>
-            <Text>{product.name}</Text>
-            <Text>{product.description}</Text>
-            <Text>{product.price}</Text>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
           </ProductDetails>
         </Product>
       )}
