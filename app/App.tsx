7d8953a7083ec1bbe8ffb4b9dc701f7c2cc83cd2
Thiago Pinho <thiagopinho05@gmail.com>
2022-12-01 19:45:09 -0300

    chore: adding intl library to android

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,8 @@
 import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
 import { Main } from "./src/Main";
-
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
 
 export default function App() {
 
@@ -15,7 +17,11 @@ export default function App() {
   }
 
   return (
-    <Main />
+    <>
+      <StatusBar style="dark" />
+      <Main />
+    </>
+
   );
 }
 
