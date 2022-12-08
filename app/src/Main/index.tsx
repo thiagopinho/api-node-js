38a825ca751d4966303b3ad4550922613c1e3a85
Thiago Pinho <thiagopinho05@gmail.com>
2022-12-08 20:56:10 -0300

    fix: creating button and function to appear only with selected table

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -5,8 +5,16 @@ import { Categories } from "../components/Categories";
 import { Menu } from "../components/Menu";
 import { Button } from "../components/Button";
 import { TableModal } from "../components/TableModal";
+import { useState } from "react";
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState("");
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -21,12 +29,21 @@ export function Main() {
       </Container>
       <Footer>
         <FooterContainer>
-          <Button onPress={() => alert("Novo Pedido")}>
-            Novo Pedido
-          </Button>
+          {
+            !selectedTable && (
+              <Button onPress={() => setIsTableModalVisible(true)}>
+                Novo Pedido
+              </Button>
+            )
+          }
+
         </FooterContainer>
       </Footer>
-      <TableModal />
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
--- app/src/components/TableModal/index.tsx
+++ app/src/components/TableModal/index.tsx
@@ -1,26 +1,44 @@
-import { Modal, TouchableOpacity } from "react-native";
+import { Modal, Platform, TouchableOpacity } from "react-native";
+import { Button } from "../Button";
 import { Close } from "../Icons/Close";
 import { Text } from "../Text";
 
 import { Overlay, ModalBody, Header, Form, TextInput } from "./styles";
+import { useState } from "react";
 
-export function TableModal() {
+interface TableModalProps {
+  visible: boolean;
+  onClose: () => void;
+  onSave: (table: string) => void;
+
+}
+
+export function TableModal({ visible, onClose, onSave }: TableModalProps) {
+  const [table, setTable] = useState("");
   return (
-    <Modal transparent>
-      <Overlay>
+    <Modal
+      visible={visible}
+      transparent
+      animationType="fade"
+    >
+      <Overlay behavior={Platform.OS === "android" ? "height" : "padding"}>
         <ModalBody>
           <Header>
             <Text weight="600">Informe a mesa</Text>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={onClose}>
               <Close color="#666" />
             </TouchableOpacity>
           </Header>
           <Form>
-            <TextInput placeholder="Número da mesa" placeholderTextColor="#666" />
+            <TextInput placeholder="Número da mesa" placeholderTextColor="#666"
+              keyboardType="number-pad"
+              onChangeText={setTable}
+            />
+            <Button onPress={() => onSave(table)} disabled={table.length === 0}>Salvar</Button>
           </Form>
         </ModalBody>
 
       </Overlay>
-    </Modal>
+    </Modal >
   );
 }
